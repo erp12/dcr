2851e74577e8ab6ce582bc54a6494d03bdf4cc7d
Xiangdong Gu <xgu@massmutual.com>
2015-08-29 22:26:13 -0400

    csv option for renderChart to reduce data transfer size

--- R/dcr_classfun.R
+++ R/dcr_classfun.R
@@ -32,9 +32,11 @@ dcrchart <- function(type, id, dimension, reduce, width, height, ..., group_name
 ##' Convert dcr object to html code
 ##' @param obj dcr chart object
 ##' @param divs whether or not to generate divisions
+##' @param csv whether or not export data in csv format, otherwise it will export as json format
+##' @param csv file filename
 ##' @export
 ##'
-html <- function(object, divs = FALSE) {
+html <- function(object, divs = FALSE, csv = FALSE, filename = NULL) {
   mhtml <- list()
   ## pre-processing
   nochain <- c()
@@ -55,8 +57,11 @@ html <- function(object, divs = FALSE) {
   ## head part
   mhtml$head <- "<script type=\"text/javascript\">"
   ## code part for data
-  mhtml$code_data <- sprintf("var data = %s;\n%s\nvar ndx = crossfilter(data);",
-                             jsonlite:::toJSON(object@data), datefmt(object@data))
+  mhtml$code_data <- sprintf("%s;\n%s\nvar ndx = crossfilter(data);",
+                             ifelse(csv,
+                                    sprintf("d3.csv('%s', function (data) {", filename),
+                                    sprintf("var data = %s", jsonlite:::toJSON(object@data))),
+                             map_fmt(object@data, csv))
   ## chart definitions
   mhtml$chart_code <- paste(sapply(object@charts, chart_def), collapse = "\n")
   mhtml$dim_code <- paste(sapply(object@charts, dimension_def), collapse = "\n")
@@ -66,7 +71,7 @@ html <- function(object, divs = FALSE) {
   ## codes for each chart
   mhtml$codes <- paste(sapply(object@charts, chartcodes, object = object), collapse = "\n")
   mhtml$nochain <- nochain
-  mhtml$tail <- "dc.renderAll();\ndc.redrawAll();\n</script>"
+  mhtml$tail <- html_tail(csv)
   mhtml <-  paste(unlist(mhtml), collapse = "\n")
   if (divs) {
     code_div <- sapply(object@charts, function(x) sprintf("<div id=\"%s\"></div>", x@id))
@@ -189,7 +194,7 @@ auto_opts <- function(e, object) {
   rmean <- attr(e@reduce, "reduceMean", exact = TRUE)
   if (!(is.null(rmean) || "valueAccessor" %in% names(e@opts))) {
     e@opts[["valueAccessor"]] <- simple_fun("d.value.avg")
-  }    
+  }
   e
 }
 
@@ -218,13 +223,45 @@ get_reduce <- function(e) {
 ## jsonlite maps Date variable in R data to characters in json data, this function finds all Date
 ## variable in R data, and generate javascript code to convert the converted date strings to date
 ## format variables in mapped json data
-datefmt <- function(data) {
-  date_vars <- sapply(names(data), function(x) class(data[[x]]))
-  date_vars <- names(date_vars)[date_vars=="Date"]
-  if (length(date_vars) == 0) return("")
-  rep_str <- sprintf("\td.%s = d3.time.format('%%Y-%%m-%%d').parse(d.%s);", date_vars, date_vars)
-  rep_str <- paste0(rep_str, collapse="\n")
-  sprintf("data.forEach(function (d) {
+# datefmt <- function(data) {
+#   date_vars <- sapply(names(data), function(x) class(data[[x]]))
+#   date_vars <- names(date_vars)[date_vars=="Date"]
+#   if (length(date_vars) == 0) return("")
+#   rep_str <- sprintf("\td.%s = d3.time.format('%%Y-%%m-%%d').parse(d.%s);", date_vars, date_vars)
+#   rep_str <- paste0(rep_str, collapse="\n")
+#   sprintf("data.forEach(function (d) {
+#           %s
+# })", rep_str)
+# }
+
+## Automatically perform format mapping between R data and json data
+## This will generate a javascript code block to do the conversion
+## factor/character: no transformation
+## integer/numeric: if csv is TRUE then convert to numeric
+## date: convert to date
+## other format: not implemented
+map_fmt <- function(data, csv = FALSE) {
+  v_class <- sapply(data, class)
+  ## date format
+  date_vars <- names(v_class)[v_class == "Date"]
+  init_code <- "var dcrx_dateFormat = d3.time.format('%Y-%m-%d');\n"
+  date_code <- sprintf("\td.%s = dcrx_dateFormat.parse(d.%s);", date_vars, date_vars)
+  ## numeric format
+  num_code <- character()
+  if (csv) {
+    num_vars <- names(v_class)[v_class %in% c("numeric", "integer")]
+    num_code <- sprintf("\td.%s = +d.%s;", num_vars, num_vars)
+  }
+  ## combine all codes and output
+  all_code <- c(date_code, num_code)
+  if (length(all_code) == 0) return("")
+  all_code <- paste0(all_code, collapse="\n")
+  sprintf("%sdata.forEach(function (d) {
           %s
-})", rep_str)
+});", init_code, all_code)
+}
+
+html_tail <- function(csv = FALSE) {
+  csv_char <- ifelse(csv, "\n});", "")
+  sprintf("dc.renderAll();\ndc.redrawAll();%s\n</script>", csv_char)
 }
--- inst/example/example.R
+++ inst/example/example.R
@@ -1,3 +1,4 @@
+library(dcr)
 ## bar plot
 data <- read.csv("inst/example/data/morley.csv")
 data <- within(data, {dist <- Speed * Run / 1000})
@@ -8,114 +9,6 @@ linchart <- dcrchart("lineChart", "test", "Run", reduceSum("dist"), width = 200,
                      xAxis = x_axis(ticks = 3), yAxis = y_axis(ticks = 5))
 mydcr + linchart
 mydcr + linchart + barchart
-
-
-
-## line chart
-linechart <- dcrchart("lineChart", "yy", Run, reduceSum(dist)) + width(768) +
-  height(480) + x_lin(c(0, 20)) + interpolate("step-before") + renderArea("TRUE") +
-  renderDataPoints(TRUE) + clipPadding(10) + yAxisLabel("This is the Y Axis!") +
-  brushOn(FALSE)
-dcr + linechart + barchart
-
-## composite chart
-data2 <- read.csv("inst/example/data/morley2.csv")
-data <- within(data, {y1 <- Speed * Run / 1000; y2 <- NA})
-data2 <- within(data2, {y1 <- NA; y2 <- Speed * Run / 1000})
-data3 <- rbind(data, data2)
-dcr <- mmdcr(data)
-chart1 <- dcrchart("lineChart", "", "", reduceSum(y1))
-chart2 <- dcrchart("lineChart", "", "", reduceSum(y2))
-chart3 <- composite("aa", "Run", chart1, chart2) + width(768) + height(480) +
-  x_lin(c(0, 20)) + brushOn(FALSE)
-dcr + chart3
-
-## example
-library(lubridate)
-data <- read.csv("inst/example/data/ndx.csv")
-data <- within(data, {
-  Loss_Gain <- ifelse(open > close, "Loss", "Gain")
-  date <- as.Date(date, "%m/%d/%Y")
-  datem <- floor_date(date, "month")
-  quarter <- quarters(date)
-  weekday <- paste0(wday(date), ".", weekdays(date, TRUE))
-  fluctuation <- round((close - open)/open*100)
-  year <- year(date)
-  absGain <- close - open
-  fluctuation <- abs(close - open)
-  Index <- (open + close)/2
-})
-mydcr <- dcr(data)
-
-mydcr + dcrchart("lineChart", "hello", "datem", reduceSum("volume"), 1000, 400, rangeChart = dc_code("chartbbb"))+
-dcrchart("barChart", "bbb", "datem", reduceSum("volume"), 900, 250,
-         round=dc_code("d3.time.month.round"), alwaysUseRounding=TRUE,
-         xUnits=dc_code("d3.time.months"),
-         margins = list(top=0, right=50, bottom=20, left = 40),
-         centerBar=TRUE, gap=1)
-
-chart1 <- dcrchart("pieChart", "chart1", "Loss_Gain", reduceCount(), width = 180, height = 180, radius = 80)
-
-chart1 <- chart1 + label_pct(chart1@chartname) + renderLabel(TRUE)
-mydcr + chart1
-
-chart2 <- dcrchart("pieChart", "chart2", "quarter", reduceSum(volume)) +
-  height(180) + radius(80) + innerRadius(30)
-dcr + chart1 + chart2
-
-chart3 <- dcrchart("rowChart", "chart3", "weekday", reduceCount()) +
-  margins(top = 20, left = 10, right = 10, bottom = 20) +
-  width(180) + height(180) + label_strip(1) +
-  ordinalColors(c('#3182bd', '#6baed6', '#9ecae1', '#c6dbef', '#dadaeb')) +
-  elasticX(TRUE) + x.ticks(4)
-dcr + chart3
-
-chart4 <- dcrchart("barChart", "chart4", "fluctuation", reduceCount()) +
-  height(180) + width(420) + margins(top = 10, right = 50, bottom = 30, left = 40) +
-  elasticY(TRUE) + centerBar(TRUE) + gap(1) + x_lin(c(-25, 25)) +
-  renderHorizontalGridLines(TRUE) + x.ticks(5) + y.ticks(5) + x.tickFormat_pct() +
-  brushOn(FALSE)
-dcr + chart4
-
-chart5 <- dcrchart("bubbleChart",
-                   "chart5",
-                   "year",
-                   reducefun(absGain = fsum("absGain"), fluctuation = fsum("fluctuation"),
-                                    Index = fsum("Index"), avgIndex = fratio("Index", "count"),
-                                    pctGain = fratio("absGain * 100", "avgIndex"),
-                                    pctfluctuation = fratio("fluctuation * 100", "avgIndex")),
-                   width = 990,
-                   height = 250,
-                   colorAccessor = pluck_key("absGain"),
-                   keyAccessor = pluck_key("absGain"),
-                   radiusValueAccessor = pluck_key("pctfluctuation"),
-                   x = x_linear(c(-2500, 2500)))
-# +
-#   width(990) + height(250) + colorAccessor(pluck("value.absGain")) + keyAccessor(pluck("value.absGain")) +
-#   valueAccessor(pluck("value.pctGain")) +
-#   radiusValueAccessor(pluck("value.pctfluctuation")) + x_lin(c(-2500, 2500)) + y_lin(c(-100, 100)) +
-#   r_lin(c(0, 4000)) +  elasticY(TRUE)
-mydcr + chart5
-
-dcr + chart1 + chart2 + chart3 + chart4 + chart5
-
-
-chart <- dcrchart("lineChart", "chart", "year",
-                  reduce(reducefun(open = fsum("open"), close = fsum("close"),
-                                   avgopen = fratio("open", "count"), avgclose = fratio("close", "count")))) +
-  x_lin(c(1980, 2015)) +  valueAccessor(pluck("value.avgopen")) +
-  stack(pluck("value.avgclose"))+
-  x.ticks(5) + brushOn(FALSE) + margins(left = 200, right = 50, top = 50, bottom = 50)+
-  width(800) + height(500) + renderArea(TRUE) + yAxisPadding(1.5) + xAxisPadding(1.5)
-dcr + chart + chart1
-
-mr <- reduce(reducefun(open = fsum("open"), close = fsum("close/2"),
-                 avgopen = fratio("open", "count"), avgclose = fratio("close", "count")))
-chart1 <- dcrchart("lineChart", "chart", "year", reduceCount())
-chart2 <- dcrchart("lineChart", "chart", "year", mr) + valueAccessor(pluck("value.count"))
-chart0 <- composite("chart0", "year", chart1, chart2) + x_lin(c(1985, 2012)) + x.ticks(4) +
-  width(800) + height(500) + brushOn(FALSE) + margins(left = 100, top = 100, right = 100, bottom = 100) + elasticY(TRUE)
-dcr + chart0
 
 ###
 data <- data.frame(name = c("apple", "orange", "banana", "grapefruit", "grapefruit"), cnt = c(10, 15, 12, 2, 4))
@@ -130,11 +23,6 @@ mydcr <- dcr(mtcars)
 
 mydcr + dcrchart("barChart", "aa", "cyl", reduceSum("am"), 500, 300)  +
   dcrchart("dataTable", "bb", "cyl", dc_code(""), group = dc_code("function(d) {return d.am}"), 800, 500, columns = c("cyl", "gear"), size = 200)
-
-##stackable table
-mydcr <- dcr(mtcars)
-mydcr + dcrchart("lineChart", "aa", "cyl", reduceSum("am"), 500, 300, stack = dc_code("aaGroup "),
-                  renderArea = TRUE, legend = dc_legend(x = 350, y=0),stack = dc_code("aaGroup,"))
 
 
 ## geo json
@@ -152,16 +40,19 @@ d
 
 ###########
 library(dcr)
-example <- data.frame(state = rep(c("MA", "CT", "NY"), each = 100), 
+example <- data.frame(state = rep(c("MA", "CT", "NY"), each = 100),
                       x = c(rnorm(100, 1), rnorm(100, 2), rnorm(100, 3)),
                       y = sample(c("A", "B", "C"), 300, replace = TRUE),
                       stringsAsFactors = FALSE)
 mydcr <- dcr(example)
-bar <- dcrchart("barChart", "myb", "y", reduceMean("x"), 300, 200)
-geochart <- dcrchart("geoChoroplethChart", "geo", "state", reduceMean("x"), 990, 500,
+bar <- dcrchart("barChart", "myb", "y", reduceMean("x"), 495, 250)
+geochart <- dcrchart("geoChoroplethChart", "geo", "state", reduceMean("x"), 480, 250,
                      overlayGeoJson = geojson("geojson/us-states.json", "state", "function(d) {return d.properties.name;}"),
-                     linearColors = c("#4575b4", "#ffffbf", "#a50026"),
-                     colorDomain = c(0, 1.33))
+                     colorCalculator = dc_code("function (d) { return d ? chartgeo.colors()(d) : '#ccc'; }"),
+                     colors = dc_code('d3.scale.quantize().range(["#E2F2FF", "#C4E4FF", "#9ED2FF", "#81C5FF", "#6BBAFF", "#51AEFF", "#36A2FF", "#1E96FF", "#0089FF", "#0061B5"])'),
+                     colorDomain = c(0, 4),
+                     projection = dc_code("d3.geo.albersUsa().scale(500).translate([240, 125])"),
+                     title = simple_fun("d.key + ':' + d.value"))
 mydcr + bar + geochart
 
 ## legend
